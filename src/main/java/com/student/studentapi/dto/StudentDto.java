713d0f9ab028c9d841709f7797160dadb323b237
Diana-nyamai <dnyamai.dn@gmail.com>
2022-11-11 11:59:11 +0300

    student dto creation

---
+++ src/main/java/com/student/studentapi/dto/StudentDto.java
@@ -0,0 +1,10 @@
+package com.student.studentapi.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+    private int studentId;
+    private String firstname;
+    private String lastname;
+}
